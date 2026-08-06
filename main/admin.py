0e21199df5fa102c23b83596e61e07a10fe86302
amal <amnerkailo@gmail.com>
2026-08-06 16:25:38 +0530

    Add Project model and register it in Django admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import project
 
 # Register your models here.
+
+@admin.register(project)
+class projectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'technologies', 'created_at')
+    search_fields = ('title', 'technologies')
+    ordering = ('-created_at',)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technologies = models.CharField(max_length=100)
+    github_url = models.URLField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
