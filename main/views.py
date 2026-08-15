0ec95fd29c0c7d7ece2e2f6c45e58e2bec1961e6
amal <amnerkailo@gmail.com>
2026-08-15 15:25:44 +0530

    Build dynamic portfolio homepage

--- main/views.py
+++ main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+from .models import project
+
 # Create your views here.
 def home(request):
-    return render(request, 'main/home.html')
+    projects = project.objects.all()
+
+    return render(
+        request,
+        'main/home.html',
+        {'projects': projects}
+    )
