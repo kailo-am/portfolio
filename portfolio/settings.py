299690be1874751e14e5e2ddcd9000a9d969a770
amal <amnerkailo@gmail.com>
2026-08-18 21:58:58 +0530

    Update production Django settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
