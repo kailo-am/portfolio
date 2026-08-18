0787b448c798c98ae1a3a55c43aa7eaf098acebf
amal <amnerkailo@gmail.com>
2026-08-18 15:39:39 +0530

    Configure production CSRF settings

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,6 +25,13 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+CSRF_TRUSTED_ORIGINS = [
+    origin for origin in os.environ.get(
+        "CSRF_TRUSTED_ORIGINS",
+        ""
+    ).split(",")
+    if origin
+]
 
 # Application definition
 
